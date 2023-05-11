cf393320012e69a1e99a26c2dd2accac52a65f83
atomic-code <danielvazquezheredia1994@gmail.com>
2023-05-11 17:50:38 +0200

    layout y react-router implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Fondo } from './components/layout/Fondo/Fondo'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
+    
     <main>
-      <Fondo></Fondo>
-      <h1>HOLII</h1>
+      <BrowserRouter>
+        <Fondo></Fondo>
+        
+        <Routes>
+          <Route path='/' element={<h1>HOLII</h1>}/>
+          <Route path='/test' element={<h1>test</h1>}/>
+        </Routes>
+
+      </BrowserRouter>
     </main>
   )
+
+  
 }
 
 export default App
