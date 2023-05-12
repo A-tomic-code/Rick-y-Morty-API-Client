0be134bd15b9f14ba292fdbc49aec131065bcdab
atomic-code <danielvazquezheredia1994@gmail.com>
2023-05-12 21:11:29 +0200

    barra superior

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -1,33 +1,46 @@
-
 import './ResultsPage.css';
+import {Link} from 'react-router-dom'
 import { ResultCard } from '../ResultCard/ResultCard';
-import {useSelector} from 'react-redux'
+import {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import {fetchData} from '../../slices/fetchDataSlice'
 
 const ResultsPage = () => {
-
-  const data = useSelector(state => state.fetchData.data)
+  const data = useSelector((state) => state.fetchData.data);
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState();
 
   return (
-    
     <div className="results-page">
-      
       <div className="search-bar">
-        <h1>Finder</h1>
+        <Link to='/'><h1>Finder</h1></Link>
 
-        <form>
-          <input type="text" name="search" placeholder="Search" />
-          <button type="button">Search</button>
+        <form onSubmit={e => {
+          e.preventDefault();
+          fetchData(dispatch, query)
+        }}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Buscar"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="button">Buscar</button>
         </form>
-
-        
       </div>
-  
+
       <div className="results-container">
-        {Object.keys(data).length 
-          ? Object.keys(data).includes('results')
-            ? data.results.map(item => <ResultCard key={item.id}data={item}></ResultCard>)
-            : <ResultCard data={data}></ResultCard>
-          : <h1>NODATA</h1> }
+        {Object.keys(data).length ? (
+          Object.keys(data).includes('results') ? (
+            data.results.map((item) => (
+              <ResultCard key={item.id} data={item}></ResultCard>
+            ))
+          ) : (
+            <ResultCard data={data}></ResultCard>
+          )
+        ) : (
+          <h1>NODATA</h1>
+        )}
       </div>
     </div>
   );
--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { fetchData } from '../../slices/fetchDataSlice';
 
 import './SearchPage.css';
@@ -8,6 +8,7 @@ import './SearchPage.css';
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState(null);
   const dispatch = useDispatch();
+  const navigate = useNavigate()
 
   return (
     <div className="search-page">
@@ -15,6 +16,8 @@ const SearchPage = () => {
         className="search-form"
         onSubmit={(e) => {
           e.preventDefault();
+          fetchData(dispatch, searchQuery);
+          navigate('/results')
         }}
       >
         <input
@@ -22,17 +25,13 @@ const SearchPage = () => {
           id="searchQuery"
           onChange={(event) => setSearchQuery(event.target.value)}
         />
-        <Link to="/results">
+
           <button
             type="button"
             className="btn-search"
-            onClick={() => {
-              fetchData(dispatch, searchQuery);
-            }}
           >
             Buscar
           </button>
-        </Link>
       </form>
     </div>
   );
